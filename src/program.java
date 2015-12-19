1222cfc4e2e66bd38501c4c98e26d1a7e596bca1
ramon54321 <ramon2009@gmail.com>
2015-12-19 15:36:44 +0200

    Did a addition to the main program class, added an extra project line.

--- src/program.java
+++ src/program.java
@@ -4,5 +4,6 @@
 public class program {
     public static void main(String args[]){
         System.out.println("Hello, this is printed from the program.");
+        System.out.println("We expanded the program now, adding this line from a new project folder.");
     }
 }
